24746a20e3b6bf0a6f9d850c13237a3147ad74ca
gissily <gissily@outlook.com>
2023-02-28 13:13:54 +0800

    fix script

--- oraclelinux/8-slim.sh
+++ oraclelinux/8-slim.sh
@@ -6,7 +6,7 @@ echo " "
 echo " "
 echo 'build oraclelinux:8-slim start'
 
-GOSU_VERSION=$(curl https://api.github.com/repos/tianon/gosu/releases/latest >> /dev/null 2>&1 | grep tag_name | cut -d '"' -f 4)
+GOSU_VERSION=$(curl https://api.github.com/repos/tianon/gosu/releases/latest | grep tag_name | cut -d '"' -f 4)
 BASE_IMAGE=oraclelinux:8-slim
 
 echo "BASE_IMAGE=[${BASE_IMAGE}] GOSU_VERSION=[${GOSU_VERSION}]"
