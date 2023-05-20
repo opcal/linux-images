da6bde480bc817542e458f108c9288c9ee590d60
gissily <gissily@outlook.com>
2023-05-20 16:15:39 +0800

    build apline 3.18

--- alpine/3.18.sh
+++ alpine/3.18.sh
@@ -4,25 +4,26 @@ set -e
 
 echo " "
 echo " "
-echo 'build alpine:3.17 start'
+echo 'build alpine:3.18 start'
 
-BASE_IMAGE=alpine:3.17
+BASE_IMAGE=alpine:3.18
 GOSU_VERSION=$(curl https://api.github.com/repos/tianon/gosu/releases/latest | grep tag_name | cut -d '"' -f 4)
 
 echo "BASE_IMAGE=[${BASE_IMAGE}] GOSU_VERSION=[${GOSU_VERSION}]"
 echo " "
 echo " "
 
-# alpine:3.17
+# alpine:3.18
 docker buildx build \
     --platform ${PLATFORM} \
     --build-arg BASE_IMAGE=${BASE_IMAGE} \
     --build-arg GOSU_VERSION=${GOSU_VERSION} \
     --push \
-    -t ${CI_REGISTRY}/opcal/alpine:3.17-${TIMESTAMP} \
-    -t ${CI_REGISTRY}/opcal/alpine:3.17 \
+    -t ${CI_REGISTRY}/opcal/alpine:3.18-${TIMESTAMP} \
+    -t ${CI_REGISTRY}/opcal/alpine:3.18 \
+    -t ${CI_REGISTRY}/opcal/alpine:latest \
     -f ${PROJECT_DIR}/alpine/base/Dockerfile . --no-cache
 
-echo 'build alpine:3.17 finished'
+echo 'build alpine:3.18 finished'
 echo " "
 echo " "
