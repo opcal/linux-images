4732e4371c0506c0960178360eed13434499d076
gissily <gissily@outlook.com>
2024-06-01 02:13:07 +0800

    alpine 3.20

--- alpine/3.20.sh
+++ alpine/3.20.sh
@@ -4,25 +4,26 @@ set -e
 
 echo " "
 echo " "
-echo 'build alpine:3.19 start'
+echo 'build alpine:3.20 start'
 
-BASE_IMAGE=alpine:3.19
+BASE_IMAGE=alpine:3.20
 GOSU_VERSION=$(curl https://api.github.com/repos/tianon/gosu/releases/latest | jq | grep tag_name | cut -d '"' -f 4)
 
 echo "BASE_IMAGE=[${BASE_IMAGE}] GOSU_VERSION=[${GOSU_VERSION}]"
 echo " "
 echo " "
 
-# alpine:3.19
+# alpine:3.20
 docker buildx build \
     --platform ${PLATFORM} \
     --build-arg BASE_IMAGE=${BASE_IMAGE} \
     --build-arg GOSU_VERSION=${GOSU_VERSION} \
     --push \
-    -t ${CI_REGISTRY}/opcal/alpine:3.19-${TIMESTAMP} \
-    -t ${CI_REGISTRY}/opcal/alpine:3.19 \
+    -t ${CI_REGISTRY}/opcal/alpine:3.20-${TIMESTAMP} \
+    -t ${CI_REGISTRY}/opcal/alpine:3.20 \
+    -t ${CI_REGISTRY}/opcal/alpine:latest \
     -f ${PROJECT_DIR}/alpine/base/Dockerfile . --no-cache
 
-echo 'build alpine:3.19 finished'
+echo 'build alpine:3.20 finished'
 echo " "
 echo " "
