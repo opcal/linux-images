9eb9faef49faa8f55258ef2473f1c9361eaf6a84
gissily <gissily@outlook.com>
2025-04-28 16:12:11 +0800

    fix bake file

--- oraclelinux/oraclelinux.sh
+++ oraclelinux/oraclelinux.sh
@@ -4,16 +4,16 @@ set -e
 
 echo " "
 echo " "
-echo 'build ubuntu start'
+echo 'build oraclelinux start'
 
 GOSU_VERSION=$(curl https://api.github.com/repos/tianon/gosu/releases/latest | jq | grep tag_name | cut -d '"' -f 4)
 
 docker buildx bake \
-    -f ${PROJECT_DIR}/ubuntu/docker-bake.hcl \
+    -f ${PROJECT_DIR}/oraclelinux/docker-bake.hcl \
     --pull \
     --push \
     --no-cache
 
-echo 'build ubuntu finished'
+echo 'build oraclelinux finished'
 echo " "
 echo " "
